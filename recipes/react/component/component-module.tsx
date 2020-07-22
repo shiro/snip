515f18ae4d6854b307f01e96c89a3fc1893198d2
shiro <shiro@usagi.io>
2020-07-22 20:19:08 +0200

    Add react component

---
+++ recipes/react/component/component-module.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+import cn from "classnames";
+
+import css from "./{{ name }}.module.scss";
+
+
+interface IProps {
+}
+
+
+const {{ name }}: React.FC<IProps> = (props) => {
+    return (
+        <div className={cn()}>
+        </div>
+    );
+};
+
+
+export default {{ name }};
